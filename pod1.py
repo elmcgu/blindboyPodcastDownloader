7e0e69823853df0c4e6255e716612a168d732196
EEJ McGuinness <96074540+KraftJunk@users.noreply.github.com>
2023-02-08 10:58:27 +0000

    Update pod1.py
    If user inputs yes when asked if they want to transcribe episode. Added function to convert mp3 file to wav before conversion and function to split wav file into chunks, save to audio chunks folder, convert it to text and add to transcription.txt file.

--- pod1.py
+++ pod1.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import speech_recognition as sr
+import os
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
 
 
 rssFeedSource="https://feeds.acast.com/public/shows/9d5c107b-68d6-4c1b-8c80-45ee6a84c947"
@@ -78,5 +82,76 @@ filename = latestTitleString+ ".mp3"
 with open(filename, 'wb') as f:
         # You will get the file in base64 as content
         f.write(r.content)
+print(latestTitleString +" has been downloaded to the location of this py file.")
+
+def convert2Wav(filename):
+    # assign files
+    inputFile = filename
+    outputFile = latestTitleString + ".wav"
+
+    # convert mp3 file to wav file
+    sound = AudioSegment.from_mp3(inputFile)
+    sound.export(outputFile, format="wav", parameters=["-ac","2","-ar","8000"])
+
+def get_large_audio_transcription(outputFile):
+    # create a speech recognition object
+    r = sr.Recognizer()
+
+    """
+    Splitting the large audio file into chunks
+    and apply speech recognition on each of these chunks
+    """
+    # open the audio file using pydub
+    sound = AudioSegment.from_wav(outputFile)
+    # split audio sound where silence is 300 miliseconds or more and get chunks
+    chunks = split_on_silence(sound,
+        # experiment with this value for your target audio file
+        min_silence_len = 300,
+        # adjust this per requirement
+        silence_thresh = sound.dBFS-14,
+        # keep the silence for 1 second, adjustable as well
+        keep_silence=300,
+    )
+    folder_name = "audio-chunks"
+    # create a directory to store the audio chunks
+    if not os.path.isdir(folder_name):
+        os.mkdir(folder_name)
+    whole_text = ""
+    # process each chunk
+    for i, audio_chunk in enumerate(chunks, start=1):
+        # export audio chunk and save it in
+        # the `folder_name` directory.
+        chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
+        audio_chunk.export(chunk_filename, format="wav")
+        # recognize the chunk
+        with sr.AudioFile(chunk_filename) as source:
+            audio_listened = r.record(source)
+            # try converting it to text
+            try:
+                text = r.recognize_google(audio_listened)
+            except sr.UnknownValueError as e:
+                print("Error:", str(e))
+            else:
+                text = f"{text.capitalize()}. "
+                print(chunk_filename, ":", text)
+                whole_text += text
+    # return the text for all chunks detected
+
+
+    with open(latestTitleString+'transcript.txt', 'w') as f:
+
+            f.write(whole_text)
+
+
+ask1=input("Would you like to transcribe the podcast episode? ")
+if ask1.lower() == "yes":
+    convert2Wav(latestTitleString + ".mp3")
+    get_large_audio_transcription(latestTitleString + ".wav")
+else:
+    exit(0)
+
+
+
+
 
 
